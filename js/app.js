afe38c04375eb1459d4e5ffc96bc5c31309f0b30
Sebastian <winigossos@gmail.com>
2025-03-11 09:24:06 -0300

    feat: enable correct category selection

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
 function initApp() {
 
+    const selectCategories = document.querySelector('#categorias');
+    selectCategories.addEventListener('change', selectCategory)
+
     getCategories();
 
     function getCategories() {
         const url = 'https://www.themealdb.com/api/json/v1/1/categories.php'
         fetch( url )
             .then( res => res.json() )
-            .then( res => {
-                console.log( res )
-            })
+            .then( showCategories )
+    }
+
+    function showCategories({ categories = [] }) {
+        categories.forEach( category => {
+
+            const { strCategory } = category;
+            const option = document.createElement('OPTION');
+            option.value = strCategory;
+            option.textContent = strCategory;
+            selectCategories.appendChild( option );
+        });
+    }
+
+    function selectCategory( e ) {
+        const category = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${ category }`;
+
+        fetch( url )
+            .then( res => res.json() )
+            .then( res => console.log(res ) )
     }
 }
 
